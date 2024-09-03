10e28408f4efc34d2dcdbf73e297003cad96df67
Salaton <nairouasalaton@gmail.com>
2024-09-03 09:08:13 +0300

    feat: add enhanced kyc workflows (#7)

--- enhanced_kyc.go
+++ enhanced_kyc.go
@@ -1,1 +1,28 @@
 package smileidentity
+
+import (
+	"context"
+	"net/http"
+)
+
+func (c *Client) EnhancedKYCAsyncVerification(ctx context.Context, input *KYCInput) (*AsyncResponse, error) {
+	var resp AsyncResponse
+
+	err := c.makeRequest(ctx, http.MethodPost, "v1/async_id_verification", nil, input, resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return &resp, nil
+}
+
+func (c *Client) EnhancedKYCVerification(ctx context.Context, input *KYCInput) (*KYCVerificationResult, error) {
+	var resp KYCVerificationResult
+
+	err := c.makeRequest(ctx, http.MethodPost, "v1/id_verification", nil, input, resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return &resp, nil
+}

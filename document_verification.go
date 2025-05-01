898e01c2314b30e55459c97ae5adbb215351e826
salaton <nairouasalaton@gmail.com>
2025-05-02 00:50:14 +0300

    chore: add request job response models

---
+++ document_verification.go
@@ -0,0 +1,22 @@
+package smileidentity
+
+import (
+	"context"
+	"net/http"
+)
+
+func (c *Client) JobRequest(ctx context.Context, input *RequestJobPayload) (*RequestJobResponse, error) {
+	var resp RequestJobResponse
+
+	input.PartnerParams.JobType = 6
+	err := c.makeRequest(ctx, http.MethodPost, "v1/upload", nil, nil, input, resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return &resp, nil
+}
+
+func (c *Client) UploadJobPayload(ctx context.Context, input *RequestJobResponse) error {
+	return nil
+}

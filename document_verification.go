d4cf9fd814a9875a972d7fef2c1217d76df38e01
Salaton <nairouasalaton@gmail.com>
2025-05-02 01:00:26 +0300

    feat: add release please to automate releases (#13)

--- document_verification.go
+++ document_verification.go
@@ -9,6 +9,7 @@ func (c *Client) JobRequest(ctx context.Context, input *RequestJobPayload) (*Req
 	var resp RequestJobResponse
 
 	input.PartnerParams.JobType = 6
+
 	err := c.makeRequest(ctx, http.MethodPost, "v1/upload", nil, nil, input, resp)
 	if err != nil {
 		return nil, err
